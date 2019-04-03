054c4630b7a67f09fd527444bffd23d6901fd588
Simuel Henderson <simmy@simuels-macbook-pro.local>
2019-04-02 21:40:42 -0500

    Install RealmSwiftPod

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -14,7 +14,6 @@ class CategoryViewController: UITableViewController {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     var categoryArray = [Category]()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         loadItems()
